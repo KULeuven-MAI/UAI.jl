0b8c0db0a04aa04f6879c9a88ad123f65394fa64
Dieter Castel <dietercastel@gmail.com>
2020-06-25 11:39:49 +0200

    Added simple interactive example.

---
+++ examples/simple.jl
@@ -0,0 +1,11 @@
+# A very simple example.
+using UAI
+j = JPD("gotDrug>health")
+
+# setAllInteractive!(j) would error because no domain is set yet.
+
+# So always set the domains first
+setDomain!(j,:gotDrug,[:drug, :noDrug])
+setDomain!(j,:health,[:healthy, :ill])
+
+setAllInteractive!(j)
